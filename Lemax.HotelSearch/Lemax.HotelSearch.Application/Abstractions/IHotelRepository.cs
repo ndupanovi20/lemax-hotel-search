cd599e17ebb43d348b6ad6f7924ece4ee4c1fd7a
Nimaj <nimaj.dupaovic@outlook.com>
2026-07-15 16:34:42 +0200

    Added IHotelRepository interface for Hotel CRUD operations in the Application.Abstractions namespace.

---
+++ Lemax.HotelSearch/Lemax.HotelSearch.Application/Abstractions/IHotelRepository.cs
@@ -0,0 +1,16 @@
+﻿using Lemax.HotelSearch.Domain.Entities;
+
+namespace Lemax.HotelSearch.Application.Abstractions;
+
+public interface IHotelRepository
+{
+    Task<IReadOnlyCollection<Hotel>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    Task<Hotel?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task AddAsync(Hotel hotel, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(Hotel hotel, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
